ffd1f56312a182e8714ad94338a95d086803a174
Brian Jimerson <bjimerson@gmail.com>
2024-01-30 16:57:21 -0500

    Add placeholder cards

--- src/preload.js
+++ src/preload.js
@@ -8,29 +8,57 @@ window.addEventListener("DOMContentLoaded", () => {
         //Create an HTML list from the clipboard history
         jsonValue = JSON.parse(value);
         let html = "";
-        for (var i = 0; i < jsonValue.histories.length; i++) {
-            var historyItem = jsonValue.histories[i];
+        let iterationLength;
+        if (jsonValue.histories.length == 0 || jsonValue.histories.length % 3 == 0) {
+            iterationLength = jsonValue.histories.length;
+        } else if (jsonValue.histories.length % 3 == 1) {
+            iterationLength = jsonValue.histories.length + 2;
+        } else {
+            iterationLength = jsonValue.histories.length + 1;
+        }
+        for (var i = 0; i < iterationLength; i++) {
             if (i == 0 || i % 3 == 0 ) {
                 //Begin new row
                 html += "<div class=\"row mb-3\">";
                 html += "<div class=\"card-group\">";
             }
-            html += "<div class=\"card\" style=\"width: 18rem;\">";
-            html += "<div class=\"card-body\">"
-            html += "<p class=\"card-text\">"
-            html += historyItem.clipboard
-            html += "</p>"
-            html += "<button class=\"btn btn-primary btn-sm\" onclick=\"writeToClipboard('";
-            html += historyItem.clipboard;
-            html += "')\">";
-            html += "Copy to clipboard";
-            html += "</button>";
-            html += "</div>";
-            html += "<div class=\"card-footer text-body-secondary\">"
-            html += getElapsedTime(historyItem.createdDate);
-            html += "</div>"
-            html += "</div>";
-            if (i == 2 || i % 3 == 2 || i == jsonValue.histories.length) {
+            if (i < jsonValue.histories.length) {
+                // Still iterating over history items; create a regular card
+                var historyItem = jsonValue.histories[i];
+                html += "<div class=\"card\" style=\"width: 18rem;\">";
+                html += "<div class=\"card-body\">"
+                html += "<p class=\"card-text\">"
+                html += historyItem.clipboard
+                html += "</p>"
+                html += "<button class=\"btn btn-primary btn-sm\" onclick=\"writeToClipboard('";
+                html += historyItem.clipboard;
+                html += "')\">";
+                html += "Copy to clipboard";
+                html += "</button>";
+                html += "</div>";
+                html += "<div class=\"card-footer text-body-secondary\">"
+                html += getElapsedTime(historyItem.createdDate);
+                html += "</div>"
+                html += "</div>";
+    
+            } else {
+                //Done with history items, create a placeholder card
+                html += "<div class=\"card\" aria-hidden=\"true\" style=\"width: 18rem;\">";
+                html += "<div class=\"card-body\">"
+                html += "<p class=\"card-text placeholder-glow\">"
+                html += "<span class=\"placeholder col-10\">"
+                html += "</span>"
+                html += "</p>"
+                html += "<button class=\"btn btn-primary btn-sm disabled placeholder col-4\">";
+                html += "</button>";
+                html += "</div>";
+                html += "<div class=\"card-footer text-body-secondary placeholder-glow\">"
+                html += "<span class=\"placeholder col-6\">"
+                html += "</span>"
+                html += "</div>"
+                html += "</div>";
+            }
+            if (i == 2 || i % 3 == 2 ) {
                 // End row
                 html += "</div>";
                 html += "</div>";
