6c739e0cb3a38193fa3cc4738286a18b397132cf
Brian Jimerson <bjimerson@gmail.com>
2024-02-02 09:42:00 -0500

    Fix ordering and update documentation

--- src/store/clipboardStore.js
+++ src/store/clipboardStore.js
@@ -1,20 +1,35 @@
 //Imports
-const electron = require("electron");
+const { app, electron } = require("electron");
 const path = require("node:path");
 const fs = require("node:fs");
 const crypto = require("node:crypto");
 
 //ClipboardStore class definition
-class ClipboardStore{
+class ClipboardStore {
 
+    /*
+    History JSON structure:
+      history = {
+          histories: [
+            item: {
+                id: uuid,
+                createdDate: Date(),
+                clipboard: string
+            }
+          ]
+      }
+    */
     history = {
         histories: []
     }
 
     //Constructor
     constructor(opts) {
-        const userDataPath = (electron.app || electron.remote.app).getPath("userData");
+        //Get file name to save history to
+        const userDataPath = app.getPath("userData");
         this.fileName = path.join(userDataPath, opts.configName + ".json");
+
+        //Make sure the configuration file exists
         fs.access(this.fileName, fs.constants.R_OK | fs.constants.W_OK, (err) => {
             console.log(`${this.fileName} ${err ? 'is not writable, creating file' : 'is writeable'}`);
             if (err) fs.writeFile(this.fileName, "", (err) => {
@@ -22,6 +37,8 @@ class ClipboardStore{
                 console.log(`Created new file ${this.fileName}`);
             })
         });
+
+        //Read existing history entries
         try {
             this.history = JSON.parse(fs.readFileSync(this.fileName)); 
         } catch (e) {
@@ -44,12 +61,12 @@ class ClipboardStore{
             createdDate: new Date(),
             clipboard: historyItem
         };
-        this.history.histories.push(item);
+        this.history.histories.unshift(item);
         fs.writeFileSync(this.fileName, JSON.stringify(this.history));
     }
 
     //Clears recent history
-    clearHistory = () =>{
+    clearHistory = () => {
         this.history.histories = [];
         fs.writeFileSync(this.fileName, JSON.stringify(this.history));
         console.log(`Cleared recent history in file ${this.fileName}`);
